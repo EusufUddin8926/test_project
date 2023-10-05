a72c02ddf4de58ae733a92c6bbab792f22b8568e
Eusuf Uddin <eusuf15-8926@diu.edu.bd>
2023-10-06 02:29:42 +0600

    test project init

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(primarySwatch: Colors.blueGrey),
+        color: Colors.black12,
+        debugShowCheckedModeBanner: false,
+        home: Homepage());
+  }
+}
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Page Screen"),
+      ),
+      body: const Text("Hello from flutter"),
+    );
+  }
+}
